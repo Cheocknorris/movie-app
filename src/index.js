cef8aa0cdcbcfb54ef2db655fbfac50ede749fa3
Eliseo Quintanilla <eliseo.quintanillagz@gmail.com>
2021-07-19 12:41:14 +0200

    First commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SearchMovies from './components/Search-movies'
+// import './index.css';
+// import reportWebVitals from './reportWebVitals';
+
+class Main extends React.Component {
+  render() {
+      return (
+        <div className='container'>
+          <h1 className='title'>Search Movie App</h1>
+          < SearchMovies />
+        </div>
+      )
+  }
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +26,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
